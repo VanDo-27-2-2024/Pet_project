d3d3de22901f6f15261739d4024660969a85817b
VanDo <vandooxx@gmail.com>
2024-05-13 12:15:52 -0700

    Add linked list with insert feature

---
+++ data_structure/linkedlist/linked_list.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+
+
+
+using namespace std;
+
+struct node
+{
+    int data;
+    node* next;
+};
+
+class linked_list
+{
+    public:
+        // support: insert a node in any valid position
+        // idea to implement: link (n - 1)node to new node, and link new node to (n + 1)node
+        void insert(int _data, int pos)
+        {
+            node* temp = new node;
+            temp->data = _data;
+            temp->next = NULL;
+
+            if (pos == 1)
+            {
+                temp->next = head;
+                head = temp;
+                return;
+            }
+
+            node* temp1 = head;
+            // position: [1] [2] [..x..] [4]
+            for (int i = 1; i < (pos - 1); i++)
+            {
+                temp1 = temp1->next;
+            }
+            temp->next = temp1->next;
+            temp1->next = temp;
+        }
+
+        void print()
+        {
+            node* temp = head;
+
+            cout << "List: " ;
+            while (temp != NULL)
+            {
+                cout << " " << temp->data;
+                temp = temp->next;
+            }
+            cout << "\n";
+        }
+
+    private:
+        node* head = NULL;
+};
+
+
+int main()
+{
+    linked_list list;
+    list.insert(1, 1);
+    list.insert(3, 2);
+    list.insert(5, 3);
+    list.insert(7, 4);
+    list.insert(9, 5);
+
+    list.print();
+
+    return 0;
+}
